ac561e0ba3d81b7db39deb306fa3c97f4b3aa2de
Bruno Szczuk <brunoszczuk@gmail.com>
2022-09-17 18:55:09 -0300

    first commit

---
+++ src/core/usecase/exams/impl/GenerateUserResultUseCaseImpl.ts
@@ -0,0 +1,8 @@
+import {GenerateUserResultUseCase} from "../../GenerateUserResultUseCase";
+
+export class GenerateUserResultUseCaseImpl implements GenerateUserResultUseCase {
+    execute(command: GenerateUserResultCommand): GenerateUserResultCommand {
+        
+    }
+
+}
